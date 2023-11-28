eb7d31f758cd22b44013f14f7d94bb39c1cc46df
Danial Cossette <m3ssee@gmail.com>
2023-11-27 21:30:16 -0500

    Inital commit.

---
+++ romanToInt.py
@@ -0,0 +1,15 @@
+# This function converts any valid roman numeral to its corresponding numerical value.
+# We can achieve this by knowing that: any smaller roman before a larger roman is considered substraction.
+# Any smaller roman after a larger roman is considered addition.
+
+def romanToInt(roman):
+    m = {"I":1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M":1000} # Maps each numeral stage (I is first stage, V is second, etc.)
+    ans = 0
+
+    for i in range(len(roman)): # Iterate over each character in the roman
+        if i < len(roman)-1 and m[roman[i]] < m[roman[i+1]]: # If there is still an element ahead of the current, and the current is less than that element...
+            ans += m[roman[i]] # Then add it to the answer
+        else:
+            ans -= m[roman[i]] # Otherwise subtract it
+    return ans 
+
